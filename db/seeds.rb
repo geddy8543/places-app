72dc5d08428fb16972c6e0a95839c0957d2a18b4
Gail Eddy <gaileddy68@gmail.com>
2021-11-19 09:50:15 -0600

    adds seeds to db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Place.create!(name: "The Map Room", address: "1949 N Hoyne Ave, Chicago")
+
+Place.create!(name: "Revolution Brewing", address: "2323 N Milwaukee Ave, Chicago")
+
+Place.create!(name: "Dovetail Brewery", address: "1800 W Belle Plaine Ave, Chicago")
+
+Place.create!(name: "Marz Community Brewing Co", address: "3630 S Iron St, Chicago")
